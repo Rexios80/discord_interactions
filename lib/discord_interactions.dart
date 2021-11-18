0dea26b88375a9f16a6200c3e093018c2f39865f
Rexios <rexios@rexios.dev>
2021-11-18 18:05:39 -0500

    Updated exports

--- lib/discord_interactions.dart
+++ lib/discord_interactions.dart
@@ -2,3 +2,4 @@ library discord_interactions;
 
 export 'src/api/discord_api.dart';
 export 'src/model/discord_model.dart';
+export 'src/validation/interaction_validator.dart';
--- lib/src/discord_interactions_base.dart
+++
@@ -1,6 +0,0 @@
-/// TODO
-///
-// TODO: Clean up imports across the codebase
-// TODO: Consistent constructor docs
-// TODO: Consistent class docs
-class DiscordInteractions {}
