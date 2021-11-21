bb7e1491adf9fd272ba26bd0a5743a69a2ff642d
Rexios <rexios@rexios.dev>
2021-11-21 01:26:19 -0500

    Added DiscordColor class

---
+++ lib/src/convenience/discord_color.dart
@@ -0,0 +1,141 @@
+/// All colors from the nyxx DiscordColor class
+/// https://github.com/nyxx-discord/nyxx/blob/1ea7a859168c6fcabfcfa4778fb1dd3d1f97c7cc/lib/src/core/DiscordColor.dart
+class DiscordColor {
+  /// Represents no color, or integer 0.
+  static const none = 0;
+
+  /// A near-black color. Due to API limitations, the color is #010101, rather than #000000, as the latter is treated as no color.
+  static const black = 0x010101;
+
+  /// White, or #FFFFFF.
+  static const white = 0xFFFFFF;
+
+  /// Gray, or #808080.
+  static const gray = 0x808080;
+
+  /// Dark gray, or #A9A9A9.
+  static const darkGray = 0xA9A9A9;
+
+  /// Light gray, or #808080.
+  static const lightGray = 0xD3D3D3;
+
+  /// Very dark gray, or #666666.
+  static const veryDarkGray = 0x666666;
+
+  /// Flutter blue, or #02569B
+  static const flutterBlue = 0x02569B;
+
+  /// Dart's primary blue color, or #0175C2
+  static const dartBlue = 0x0175C2;
+
+  /// Dart's secondary blue color, or #13B9FD
+  static const dartSecondary = 0x13B9FD;
+
+  /// Discord Blurple, or #7289DA.
+  static const blurple = 0x7289DA;
+
+  /// Discord Grayple, or #99AAB5.
+  static const grayple = 0x99AAB5;
+
+  /// Discord Dark, But Not Black, or #2C2F33.
+  static const darkButNotBlack = 0x2C2F33;
+
+  /// Discord Not QuiteBlack, or #23272A.
+  static const notQuiteBlack = 0x23272A;
+
+  /// Red, or #FF0000.
+  static const red = 0xFF0000;
+
+  /// Dark red, or #7F0000.
+  static const darkRed = 0x7F0000;
+
+  /// Green, or #00FF00.
+  static const green = 0x00FF00;
+
+  /// Dark green, or #007F00.
+  static const darkGreen = 0x007F00;
+
+  /// Blue, or #0000FF.
+  static const blue = 0x0000FF;
+
+  /// Dark blue, or #00007F.
+  static const darkBlue = 0x00007F;
+
+  /// Yellow, or #FFFF00.
+  static const yellow = 0xFFFF00;
+
+  /// Cyan, or #00FFFF.
+  static const cyan = 0x00FFFF;
+
+  /// Magenta, or #FF00FF.
+  static const magenta = 0xFF00FF;
+
+  /// Teal, or #008080.
+  static const teal = 0x008080;
+
+  /// Aquamarine, or #00FFBF.
+  static const aquamarine = 0x00FFBF;
+
+  /// Gold, or #FFD700.
+  static const gold = 0xFFD700;
+
+  /// Goldenrod, or #DAA520
+  static const goldenrod = 0xDAA520;
+
+  /// Azure, or #007FFF.
+  static const azure = 0x007FFF;
+
+  /// Rose, or #FF007F.
+  static const rose = 0xFF007F;
+
+  /// Spring green, or #00FF7F.
+  static const springGreen = 0x00FF7F;
+
+  /// Chartreuse, or #7FFF00.
+  static const chartreuse = 0x7FFF00;
+
+  /// Orange, or #FFA500.
+  static const orange = 0xFFA500;
+
+  /// Purple, or #800080.
+  static const purple = 0x800080;
+
+  /// Violet, or #EE82EE.
+  static const violet = 0xEE82EE;
+
+  /// Brown, or #A52A2A.
+  static const brown = 0xA52A2A;
+
+  /// Hot pink, or #FF69B4
+  static const hotPink = 0xFF69B4;
+
+  /// Lilac, or #C8A2C8.
+  static const lilac = 0xC8A2C8;
+
+  /// Cornflower blue, or #6495ED.
+  static const cornflowerBlue = 0x6495ED;
+
+  /// Midnight blue, or #191970.
+  static const midnightBlue = 0x191970;
+
+  /// Wheat, or #F5DEB3.
+  static const wheat = 0xF5DEB3;
+
+  /// Indian red, or #CD5C5C.
+  static const indianRed = 0xCD5C5C;
+
+  /// Turquoise, or #30D5C8.
+  static const turquoise = 0x30D5C8;
+
+  /// Sap green, or #507D2A.
+  static const sapGreen = 0x507D2A;
+
+  /// Phthalo blue, or #000F89.
+  static const phthaloBlue = 0x000F89;
+
+  /// Phthalo green, or #123524.
+  static const phthaloGreen = 0x123524;
+
+  /// Sienna, or #882D17.
+  static const sienna = 0x882D17;
+}
