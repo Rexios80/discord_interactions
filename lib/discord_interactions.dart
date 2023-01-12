62dee4d32a00f3dc3a1890d1700e4684c456f2d3
Rexios <rexios@rexios.dev>
2023-01-12 02:35:21 -0500

    Export dio_response_validator

--- lib/discord_interactions.dart
+++ lib/discord_interactions.dart
@@ -1,5 +1,7 @@
 library discord_interactions;
 
+export 'package:dio_response_validator/dio_response_validator.dart';
+
 export 'src/api/discord_api.dart';
 export 'src/model/discord_model.dart';
 export 'src/validation/interaction_validator.dart';
