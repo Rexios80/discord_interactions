bbd27868bbaddff8d40b5754579c9e515a13c1e3
Rexios <rexios@rexios.dev>
2023-01-12 02:19:27 -0500

    ...

--- tool/drv_converter.dart
+++ tool/drv_converter.dart
@@ -4,28 +4,16 @@ void main() {
   final files =
       Directory('lib/src/api/src').listSync(recursive: true).whereType<File>();
 
-  final typeRegex = RegExp(r'DiscordResponse<(.*)>');
-  final callRegex = RegExp(
-    r'validateApiCall\(\s*_dio.(.*?)\((.*?)\),\s*responseTransformer: (.*?),\s*\);',
-    dotAll: true,
-  );
-
   for (final file in files) {
     final contents = file.readAsStringSync();
 
     final newContents =
         // ignore: prefer_interpolation_to_compose_strings
         "import 'package:dio_response_validator/dio_response_validator.dart';\n" +
-            contents
-                .replaceAllMapped(
-                  typeRegex,
-                  (m) => 'ValidatedResponse<Map<String, dynamic>, ${m[1]}>',
-                )
-                .replaceAllMapped(
-                  callRegex,
-                  (m) =>
-                      '_dio.${m[1]}<Map<String, dynamic>>(${m[2]}).validate(transform: ${m[3]});',
-                );
+            contents.replaceAllMapped(
+              RegExp(r'\(data\) => (.+?).fromJson\(data\)'),
+              (m) => '${m[1]}.fromJson',
+            );
 
     file.writeAsStringSync(newContents);
   }
